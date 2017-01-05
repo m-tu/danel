62b5b00ec8bf39a5e8ea59c053edf836429e5a74
m-tu <marten@plumbr.eu>
2017-01-05 23:59:04 +0200

    Very basic piece movement

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,8 @@
 export default function Board() {
 	this.cells = [];
+	this.turn = 1;
+	this.currentPiece = null;
+
 	this.create();
 }
 
@@ -32,7 +35,7 @@ Board.prototype.create = function () {
 			if ((i + j) % 2 !== 0 && j < 3) {
 				div.classList += ' piece black';
 				cell.piece = {
-					color: 'black',
+					player: 2,
 					type: 'peasant'
 				}
 			}
@@ -40,7 +43,7 @@ Board.prototype.create = function () {
 			if ((i + j) % 2 !== 0 && j > 4) {
 				div.classList += ' piece white';
 				cell.piece = {
-					color: 'white',
+					player: 1,
 					type: 'peasant'
 				}
 			}
@@ -50,6 +53,19 @@ Board.prototype.create = function () {
 					console.log("click: ", i, j, typeof i);
 
 					let cell = this.cells[i][j];
+
+					if (this.currentPiece == null && cell.piece && cell.piece.player == this.turn) {
+						this.currentPiece = Object.assign({}, cell.piece);
+						delete cell.piece;
+						cell.el.classList.remove('piece', this.turn == 1 ? 'white' : 'black');
+					} else if(this.currentPiece && !cell.piece) {
+						cell.piece = this.currentPiece;
+						this.currentPiece = null;
+
+						cell.el.classList.add('piece', this.turn == 1 ? 'white' : 'black');
+
+						this.turn = this.turn == 1 ? 2 : 1;
+					}
 			});
 
 			col.appendChild(div);
