23f83dfc0862c29825e9b3db852f5c60140edfca
m-tu <marten@plumbr.eu>
2017-01-23 21:56:43 +0200

    Setting up players

--- src/main.js
+++ src/main.js
@@ -3,6 +3,43 @@ import css from './css/main.css';
 import io from './server/node_modules/socket.io-client';
 
 let socket = io();
+let userName;
+
+socket.on('setup', function(msg){
+	if(!userName) {
+		userName = window.prompt('Enter name');
+	}
+	console.log("name: ", userName, socket.id);
+
+	if(userName) {
+		socket.emit('setup', {
+			player: userName,
+			id: socket.id || null
+		});
+	} else {
+		window.alert('No name entered -  playing single player');
+	}
+});
+
+socket.on('newPlayer', function(players){
+	console.log("PLAYERS: ", players);
+	let playerList = document.getElementById('player-list')
+	playerList.innerHTML = '';
+	if(players.length > 1) {
+		players.forEach( player => {
+			if(player !== userName) {
+				let li = document.createElement("div");
+				li.innerHTML = player;
+				li.addEventListener('click', askToPlay);
+				playerList.appendChild(li);
+			}
+		});
+	}
+});
+
+function askToPlay(e) {
+	console.log("ask to play: ", e.target.innerHTML);
+}
 
 socket.on('move', function(move){
 	console.log("move: ", move);
--- src/server/index.js
+++ src/server/index.js
@@ -11,14 +11,61 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
-io.on('connection', function(socket){
-	console.log('a user connected');
+let clients = {};
 
-	socket.on('move', function(move){
-		console.log('move: ', move);
-		io.emit('move', 'hello everybody');
+io.on('connection', function(socket){
+	console.log('a user connected: %s, nr: %s', socket.id, Object.keys(clients).length);
+	clients[socket.id] = { sokk: socket};
+
+	socket.on('disconnect', function(){
+		if (clients[socket.id]) {
+			delete clients[socket.id];
+			console.log("Disconnected client: ", socket.id);
+		} else {
+			console.log("ERROR: Could not disconnect client: ", socket.id);
+		}
 	});
+
+	socket.on('setup', setup);
+	socket.on('move', move);
+	socket.on('startGame', startGame);
+
+	socket.emit('setup', {"hello": 'world'})
 });
+
+function setup(msg) {
+	console.log("SETUP: ", msg);
+	if(msg.id && clients[msg.id]) {
+		clients[msg.id].player = msg.player;
+		clients[msg.id].status = 0; // 0 - idle 1 - in game
+		io.emit('newPlayer', getPlayers());
+	} else {
+		console.log("SEtup failed: ", msg);
+	}
+}
+
+function move(msg){
+	console.log('move: ', msg);
+	// io.emit('move', 'hello everybody');
+}
+
+function startGame(msg) {
+	if(msg.id) {
+
+	} else {
+		console.log("Cannot start game, no id in msg: ", msg);
+	}
+}
+
+function getPlayers() {
+	let names = [];
+	for(const client in clients) {
+		if (clients[client].player) {
+			names.push(clients[client].player);
+		}
+	}
+	return names;
+}
 
 http.listen(3210, function(){
 	console.log('listening on *:3210');
